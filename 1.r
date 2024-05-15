dc7f4178364736e641e21b6dedc912639a93a05a
Vishal Patel <30061308@westernsydney.edu.au>
2024-05-15 18:51:45 +1000

    R file added

---
+++ 1.r
@@ -0,0 +1,6 @@
+setwd('c:/Users/30061308/tmp_git/')
+data=read.csv('epiSEIHCRD_combAge.csv')
+data$t=as.Date(data$t,origin = '2020-03-01')
+
+data_sub=subset(data,subset = (t>='2020-07-01' & t<='2020-12-31'))
+plot(data$t,data$H)
