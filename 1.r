f83938b1f32ae2ddf25a63919bdbca490cee1dbd
Vishal Patel <30061308@westernsydney.edu.au>
2024-05-15 18:53:32 +1000

    second version

--- 1.r
+++ 1.r
@@ -3,4 +3,4 @@ data=read.csv('epiSEIHCRD_combAge.csv')
 data$t=as.Date(data$t,origin = '2020-03-01')
 
 data_sub=subset(data,subset = (t>='2020-07-01' & t<='2020-12-31'))
-plot(data$t,data$H)
+plot(data$H,data$t)
